519cf1d0a8ff10414857fd344a6228143c7ad36a
Scott Rice <scottrice10@gmail.com>
2014-12-20 00:39:49 -0800

    Refactor using data, enter, and exit in d3.

--- watchout.js
+++ watchout.js
@@ -6,6 +6,23 @@ var gameOptions = {
   nEnemies: 30,
   padding: 20
 };
+
+var gameStats = {
+  score: 0,
+  bestScore: 0
+};
+
+var createEnemies = function() {
+  var empty = Array.apply(null, Array(gameOptions.nEnemies));
+  return empty.map(function(value, i) {
+    return {
+      id: i,
+      x: Math.random() * 100,
+      y: Math.random() * 100
+    };
+  });
+};
+
 var gameBoard = d3.select('body').append('div')
   .attr('class', 'game-board')
   .style("height", gameOptions.height + "px")
@@ -14,30 +31,31 @@ var gameBoard = d3.select('body').append('div')
     'width': gameOptions.width + 'px'
   });
 
-for (var i = 0; i < gameOptions.nEnemies; i++) {
-  d3.select('.game-board').append('div')
-    .attr('class', 'enemy')
-    .style('background-image', 'url(asteroid.png)')
-    .style('top', Math.random() * (gameOptions.height - 35) + 'px')
-    .style('left', Math.random() * (gameOptions.width - 35) + 'px');
-}
-
-
 var moveEnemies = function() {
-  d3.selectAll('.enemy')
+  var empty = createEnemies();
+  var enemies = gameBoard.selectAll('div').data(empty, function(d) {
+    return d.id;
+  });
+
+  enemies.enter().append("div")
+    .style("background-image", "url('asteroid.png')")
+    .attr('class', 'enemy');
+
+  enemies.style('top', function(enemy){
+      return Math.random() * (gameOptions.height - 35) + 'px'
+    })
+    .style('left', function(enemy){
+      return Math.random() * (gameOptions.width - 35) + 'px'
+    })
     .on("mouseover", function() {
-      onCollision();
-    })
-    .on("mouseenter", function() {
-      onCollision();
-    })
-    .on("mousemove", function() {
       onCollision();
     })
     .transition().duration(500)
     .transition().duration(2000)
     .tween('custom', tweenWithCollisionDetection);
-}
+
+  enemies.exit().remove()
+};
 
 var checkCollision = function(enemy, collidedCallback) {
   //   var radiusSum = parseFloat(enemy.style('width')) / 2;
@@ -54,16 +72,31 @@ var checkCollision = function(enemy, collidedCallback) {
 };
 
 var onCollision = function() {
-  console.log("collided");
-  // updateBestScore();
-  // gameStats.score = 0;
-  // return updateScore();
   d3.select('.game-board').style('background-color', 'red');
 
   setTimeout(function() {
     d3.select('.game-board').style('background-color', 'gray');
   }, 500);
+
+  updateBestScore();
+  gameStats.score = 0;
+  return updateScore();
 };
+
+var updateBestScore = function() {
+  gameStats.bestScore = Math.max(gameStats.bestScore, gameStats.score);
+  return d3.select('#high').text(gameStats.bestScore.toString());
+};
+
+var updateScore = function() {
+  return d3.select('#current').text(gameStats.score.toString());
+};
+
+var increaseScore = function() {
+  gameStats.score += 1;
+  return updateScore();
+};
+setInterval(increaseScore, 50);
 
 var tweenWithCollisionDetection = function(endData) {
   var enemy = d3.select(this);
@@ -76,7 +109,6 @@ var tweenWithCollisionDetection = function(endData) {
     top: Math.random() * (gameOptions.height - 35)
   };
   return function(t) {
-    var enemyNextPos;
     checkCollision(enemy, onCollision);
     var enemyNextPos = {
       left: startPos.left + (endPos.left - startPos.left) * t,
@@ -84,7 +116,7 @@ var tweenWithCollisionDetection = function(endData) {
     };
     return enemy.style('left', enemyNextPos.left + 'px').style('top', enemyNextPos.top + 'px');
   };
-}
+};
 
 setInterval(function() {
   moveEnemies();
